65e46fc7a51243ba66cadd4247107635e6aa39ae
unknown <oluwatobi.ogungbile@yahoo.com>
2024-01-23 14:05:11 +0100

    Items of the pages modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Dashboard from './pages/Dashboard.jsx';
+import Customer from './pages/Customer.jsx';
+import Settings from './pages/Settings.jsx';
+import Invoice from './pages/Invoice.jsx';
 
-
-import "./App.css";
-
-function App() {
-  return <></>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/customer" element={<Customer />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/invoice" element={<Invoice />} />
+        </Routes>
+      </Sidebar>
+    </BrowserRouter>
+  );
+};
 
 export default App;
