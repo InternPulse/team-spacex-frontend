d5aaff0b66527cf3b719952f6a29122a0f7886ce
HENRY <henryuks129@gmail.com>
2024-01-23 16:25:48 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-
-
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./layouts/Navbar";
+import Footer from "./layouts/Footer";
 
 function App() {
-  return <></>;
+  return <>
+  <BrowserRouter>
+  <Navbar/>
+   <Routes>
+
+   </Routes>
+   <Footer/>
+  </BrowserRouter>
+  </>;
 }
 
 export default App;
