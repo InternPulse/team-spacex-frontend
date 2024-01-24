ae5e26a441cf6d88995e84d733b2ac8d659ed3d2
yusuf <faladeyusuf54@gmail.com>
2024-01-24 11:31:46 +0100

    added the companies component

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,6 +3,12 @@ import RightLogoImg from "./logo_right.svg";
 import HeroImageOne from "./Hero_1.jpg";
 import HeroImageTwo from "./Hero_2.jpg";
 import HeroImageThree from "./Hero_3.jpg";
+import InvoiceImage from "./invoiceImage.svg";
+import CompanyLogo1 from "./company_logo_1.svg";
+import CompanyLogo2 from "./company_logo_2.svg";
+import CompanyLogo3 from "./company_logo_3.svg";
+import CompanyLogo4 from "./company_logo_4.svg";
+import CompanyLogo5 from "./company_logo_5.svg";
 
 export {
   LeftLogoImg,
@@ -10,4 +16,10 @@ export {
   HeroImageOne,
   HeroImageTwo,
   HeroImageThree,
+  InvoiceImage,
+  CompanyLogo1,
+  CompanyLogo2,
+  CompanyLogo3,
+  CompanyLogo4,
+  CompanyLogo5,
 };
--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import {
+  CompanyLogo1,
+  CompanyLogo2,
+  CompanyLogo3,
+  CompanyLogo4,
+  CompanyLogo5,
+} from "../assets";
+import "../css/companies.css";
 
 const Companies = () => {
-  return <div>Companies</div>;
+  return (
+    <section className="companies">
+      <p className="companies-text">
+        Trusted by Businesses for all Their invoice needs
+      </p>
+
+      <div className="companies-logos">
+        <div>
+          <img src={CompanyLogo1} alt="logo" />
+        </div>
+
+        <div>
+          <img src={CompanyLogo2} alt="logo" />
+        </div>
+
+        <div>
+          <img src={CompanyLogo3} alt="logo" />
+        </div>
+
+        <div>
+          <img src={CompanyLogo4} alt="logo" />
+        </div>
+        <div>
+          <img src={CompanyLogo5} alt="logo" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Companies;
